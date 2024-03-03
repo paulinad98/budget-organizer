7c1ea8984446ce685d9957a9c8735f627491bc4d
Paulina <paulinaklempka@gmail.com>
2024-03-03 16:37:23 +0100

    Add storybook

---
+++ stories/button.stories.ts
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+export default {
+  component: Button,
+  title: 'Button',
+}
+
+export const Default = {
+  args: {
+    variant: 'default',
+    size: 'default',
+    children: 'Default',
+  },
+}
