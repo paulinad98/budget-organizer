65e6ba0aa1d0e9c0555e27ba36d49d34668a534d
Paulina <paulinaklempka@gmail.com>
2024-03-09 19:44:38 +0100

    Add TransationBox

--- app/transaction/all/page.tsx
+++ app/transaction/all/page.tsx
@@ -1,9 +1,27 @@
 import { ExpensesSummary } from '@/components/transactions/ExpensesSummary/ExpensesSummary'
+import { TransactionBox } from '@/components/transactions/TransactionBox/TransactionBox'
+import { Typography } from '@/components/ui/Typography/Typography'
 
 export default function TransactionAll() {
   return (
     <>
       <ExpensesSummary />
+
+      <Typography className="mb-4 mt-6" variant="h2">
+        All My Expenses
+      </Typography>
+
+      <div className="space-y-4">
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+        <TransactionBox />
+      </div>
     </>
   )
 }
---
+++ components/transactions/TransactionBox/TransactionBox.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils'
+import FeatherIcon from 'feather-icons-react'
+import { Typography } from '@/components/ui/Typography/Typography'
+
+type TransactionBoxProps = React.BaseHTMLAttributes<HTMLElement>
+
+export function TransactionBox({ className, ...props }: TransactionBoxProps) {
+  return (
+    <button
+      className={cn(
+        'flex w-full items-center gap-4 rounded-md bg-surface px-4 py-3 text-left',
+        className
+      )}
+      {...props}
+    >
+      <div className="h-full rounded-md bg-light-background p-3">
+        <FeatherIcon className="w-5 text-light-icon" icon="shopping-bag" />
+      </div>
+
+      <div className="space-y-1">
+        <Typography className="text-text" variant="lead">
+          Grocery Shopping
+        </Typography>
+        <Typography className="text-secondary" variant="small">
+          22 July 2021
+        </Typography>
+      </div>
+
+      <Typography className="ml-auto text-light-error" variant="lead">
+        $-300.49
+      </Typography>
+    </button>
+  )
+}
